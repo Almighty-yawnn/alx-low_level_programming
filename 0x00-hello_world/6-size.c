b7afb6b5b4cedb2b5ad432e870e4fe63edd0a1f8
Herbert <herbertpreezy123@gmail.com>
2023-02-16 13:31:42 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,3 +17,4 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+}
