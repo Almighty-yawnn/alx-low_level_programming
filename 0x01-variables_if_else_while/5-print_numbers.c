e76dd02986f16ae06c702921bffadba2b6ed9115
Herbert <herbertpreezy123@gmail.com>
2023-02-17 17:49:31 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+/**
+ * main - A program that prints single digit of base ten
+ * Return: Always
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
