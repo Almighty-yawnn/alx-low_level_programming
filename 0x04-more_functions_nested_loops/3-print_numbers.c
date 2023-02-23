c9db31161a32f948af8959a7f1aa21ee188650b3
Herbert <herbertpreezy123@gmail.com>
2023-02-23 07:59:04 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function to print numbers
+ * Return: Always
+ */
+
+void print_numbers(void)
+{
+int c;
+
+	for (c >= 48; c <= 57; c++)
+	{
+	_putchar(c + '0');
+	}
+	_putchar("\n");
+}
