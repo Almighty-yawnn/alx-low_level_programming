1acfa3f1f1af9bae15350ec89169b457526a45bf
Almighty-yawnn <hebertmantey@gmail.com>
2023-04-10 17:43:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,43 @@
 #include "main.h"
-
-
 /**
  * read_textfile - Function to read a text file and print it.
  * @filename: The pointer to the file name
  * @letters: The number of letters to read and print
  * Return: The actual number of the letters, or 0 if  filename is NULL
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char b[1024];
-	int file_d;
-	ssize_t num_track = 0, num_read = 0;
+	char *b;
+	FILE *file_d;
+	ssize_t num_track, num_read;
 
 	if (filename == NULL)
+		return (0);
+
+	file_d = fopen(filename, "r");
+	if (file_d == NULL)
+		return (0);
+	b = malloc(letters + 1);
+	if (b == NULL)
 	{
+		fclose(file_d);
 		return (0);
 	}
-
-	file_d = open(filename, "r");
-
-	if (file_d == -1)
+	num_read = fread(b, sizeof(char), letters, file_d);
+	if (num_read == 0)
 	{
+		free(b);
+		fclose(file_d);
 		return (0);
 	}
-
-	while ((num_read = read(file_d, b, 1024)) > 0 && num_read < (ssize_t)letters)
+	num_track = fwrite(b, sizeof(char), num_read, stdout);
+	if (num_track != num_read)
 	{
-		if (write(STDOUT_FILENO, b, num_read) != num_read)
-		{
-			close(file_d);
-			return (0);
-		}
-		num_track = num_track + num_read;
+		free(b);
+		fclose(file_d);
+		return (0);
 	}
-	close(file_d);
-	return (num_track);
+	free(b);
+	fclose(file_d);
+	return (num_read);
 }
